ee64231e0ee92d1a2c6fac3ca2e6fbd513f9d906
Mertkan <mertkansen@gmail.com>
2020-08-28 11:04:31 +0300

    initial start

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-I3zGtKXZ4hKXgBz_EeAfdLCouEYsiE0",
+    authDomain: "airbnb-clone-ms.firebaseapp.com",
+    databaseURL: "https://airbnb-clone-ms.firebaseio.com",
+    projectId: "airbnb-clone-ms",
+    storageBucket: "airbnb-clone-ms.appspot.com",
+    messagingSenderId: "683752048884",
+    appId: "1:683752048884:web:5170ddfeb32892b9f636a1",
+    measurementId: "G-TD8V54L3RP"
+  };
